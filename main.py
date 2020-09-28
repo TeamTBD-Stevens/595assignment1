d174c9208813a59bf166f4cfe8981bed7c15666b
shiraz bheda <sbheda@stevens.edu>
2020-09-27 23:59:32 -0400

    added the tangent function

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ y_axis_sin, y_axis_cos = [], []
 for x in x_axis:
     y_axis_sin.append(numpy.sin(x))
     y_axis_cos.append(numpy.cos(x))
+    y_axis_tan.append(numpy.tan(x))
 
 plt.figure(1)
 plt.title("Trigonometric function")
 plt.plot(x_axis, y_axis_sin, color="pink", label="sin")
 plt.plot(x_axis, y_axis_cos, color="orange", label="cos")
+plt.plot(x_axis, y_axis_tan, color="darkseagreen", label="tan")
+
 plt.xticks([0, 0.5*pi, pi, 1.5*pi, 2*pi])
 plt.ylim(-2, 2)
 plt.hlines(0, 0, 2*pi, colors="grey", linestyles="dashed")
@@ -23,3 +26,5 @@ if __name__ == "__main__":
     plt.show()
 
 # Please make the third curve "darkseagreen", thank you
+
+# *Third curve has been added*
